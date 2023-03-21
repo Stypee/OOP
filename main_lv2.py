b1421570e0ccdaf5a0187b5ab589c032f5800518
Stypee <skaruza@tvz.hr>
2023-03-21 12:43:03 +0100

    lv2

---
+++ main_lv2.py
@@ -0,0 +1,7 @@
+kolegij = {
+}
+
+kolegij['ime_kolegija'] = input('Unesite ime kolegija: ')
+kolegij['broj_ects'] = input('Unesite ECTS bodove za kolegij: ')
+
+print('Kolegij',kolegij['ime_kolegija'],'nosi',kolegij['broj_ects'],'ECTS bodova')
