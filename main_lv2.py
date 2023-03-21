dd7bd5f388bb9f13f3171db4a2e1f90accdc74f1
Stypee <skaruza@tvz.hr>
2023-03-21 15:55:15 +0100

    lv2 konacna verzija

--- main_lv2.py
+++ main_lv2.py
@@ -1,7 +1,35 @@
+from datetime import date
+
 kolegij = {
 }
 
-kolegij['ime_kolegija'] = input('Unesite ime kolegija: ')
-kolegij['broj_ects'] = input('Unesite ECTS bodove za kolegij: ')
+kolegij['ime'] = input('Unesite ime kolegija: ').upper()
+kolegij['broj_ects'] = int(input('Unesite ECTS bodove za kolegij: '))
 
-print('Kolegij',kolegij['ime_kolegija'].upper(),'nosi',kolegij['broj_ects'],'ECTS bodova')
+# print('Kolegij',kolegij['ime'],'nosi',kolegij['broj_ects'],'ECTS bodova')
+
+ispit = {
+
+}
+
+dan = int(input('Unesite dan ispita: '))
+mjesec = int(input('Unesite mjesec ispita: '))
+godina = int(input('Unesite godinu ispita: '))
+ispit['datum'] = date(godina,mjesec,dan)
+ispit['kolegij'] = kolegij
+
+# print('Kolegij',ispit['kolegij']['ime'],'nosi',ispit['kolegij']['broj_ects'],'ECTS bodova.')
+# print('Datum ispita je:',ispit['datum'])
+
+student = {
+
+}
+
+student['ime'] = input('Unesite ime studenta: ').capitalize()
+student['prezime'] = input('Unesite ime studenta: ').capitalize()
+student['kolegij'] = ispit['kolegij']['ime']
+student['ispit'] = ispit
+
+print('Student',student['ime'],student['prezime'],'je prijavio ispit iz kolegija',student['kolegij'],
+      'koji će se održati',student['ispit']['datum'])
+
